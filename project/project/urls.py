afe96b2df1de53d78fc2be14be9e51b40b3d0ba2
lorens <lorens32645@gmail.com>
2023-03-07 00:12:47 +0300

    api

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from orders.views import orders_page
+from orders.views import orders_page, OrderView
+
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+
+router.register("api/orders", OrderView)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", orders_page),
 ]
+urlpatterns += router.urls
